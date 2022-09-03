621f05ff0b0263892f28abd57251ce8cd697ccd8
Elias Khoury <eliaskhouryy@hotmail.com>
2022-09-03 15:47:55 +1000

    edit

--- src/components/Taskr/Dashboard.js
+++ src/components/Taskr/Dashboard.js
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { useUserAuth } from '../context/UserAuthContext';
 import { useNavigate } from 'react-router';
+import { collection, getDocs, addDoc, updateDoc, doc } from 'firebase/firestore';
+import CreateProject from './CreateProject';
+import '../Authentication/LandingPage.scss';
+import { db } from '../firebase';
 
 export default function Dashboard() {
+	const [projectDetails, setProjectDetails] = useState([]);
 	const { logOut, user } = useUserAuth();
 	const navigate = useNavigate();
+	const projectsRef = collection(db, 'projects');
+	const [modal, setModal] = useState(false);
 
-	useEffect(() => {}, []);
+	useEffect(() => {
+		const getProjectDetails = async () => {
+			const data = await getDocs(projectsRef);
+			setProjectDetails(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		};
+		getProjectDetails();
+	}, []);
 
 	const handleLogout = async () => {
-		// Firebase function to handle logout
 		try {
 			await logOut();
 			navigate('/');
@@ -21,6 +33,32 @@ export default function Dashboard() {
 		<div>
 			<h1>Dashboard page</h1>
 			<button onClick={handleLogout}>Logout</button>
+			<h2>Projects</h2>
+			<button className="btn-modal" onClick={() => setModal(!modal)}>
+				Add Project
+			</button>
+
+			{projectDetails.map((project) => {
+				return (
+					<div className="projectsContainer">
+						<h3>{user.uid === project.userId ? project.title : ''}</h3>
+					</div>
+				);
+			})}
+			<div>
+				{modal && (
+					<div className="modal">
+						<div className="overlay">
+							<div className="modal-content">
+								<button className="close-modal" onClick={() => setModal(!modal)}>
+									X
+								</button>
+								<CreateProject />
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }

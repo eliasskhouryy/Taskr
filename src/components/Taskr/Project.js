6efd887f1da85dad87135fa05e30489d8cf482c3
Elias Khoury <eliaskhouryy@hotmail.com>
2022-09-03 17:44:13 +1000

    git push

--- src/components/Taskr/Project.js
+++ src/components/Taskr/Project.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useUserAuth } from '../context/UserAuthContext';
 import { collection, getDocs, addDoc, updateDoc, doc } from 'firebase/firestore';
 import { db } from '../firebase';
+import './project.scss';
 
 const Project = () => {
 	let { id } = useParams();
-	const [AddItem, setAddItem] = useState([]);
-	const [Brain, setBrain] = useState(['CHicken', 'Eggs']);
-	const [Ongoing, setOngoing] = useState([]);
-	const [Completed, setCompleted] = useState([]);
+	const { logOut, user } = useUserAuth();
+	const [title, setTitle] = useState('');
+	const [comment, setComment] = useState('');
+	const [tasks, setTasks] = useState([]);
+	const [newTask, setNewTask] = useState('');
 	const projectsRef = collection(db, 'projects');
+	const tasksRef = collection(db, 'tasks');
 	const [projectDetails, setProjectDetails] = useState([]);
+
+	const addTask = async () => {
+		await addDoc(tasksRef, { projectId: id, title: title, comment: comment, status: false });
+		window.location.reload();
+	};
 
 	useEffect(() => {
 		const getProjectDetails = async () => {
@@ -18,8 +27,12 @@ const Project = () => {
 			setProjectDetails(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
 		};
 		getProjectDetails();
+		const getTaskDetails = async () => {
+			const data = await getDocs(tasksRef);
+			setTasks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		};
+		getTaskDetails();
 	}, []);
-	console.log(id);
 
 	const Brainstorming = (Brain) => {
 		return (
@@ -37,34 +50,23 @@ const Project = () => {
 			{projectDetails.map((project) => {
 				return <h1>{project.id === id ? project.title : ''}</h1>;
 			})}
-
-			<label>Project name</label>
-			<input type="text" name="project" placeholder="Project Name" />
-
-			<div className="tasks">
-				<div className="header">
-					<form onSubmit={() => setBrain(Brain)}>
-						<input placeholder="Brainstorming" />
-						<button type="submit">Add Idea</button>
-					</form>
-				</div>
-				<div className="header">
-					<form onSubmit={() => setAddItem(AddItem)}>
-						<input placeholder="New Task" />
-						<button type="submit">Add Task</button>
-					</form>
-				</div>
-				<div className="header">
-					<form onSubmit={() => setOngoing(Ongoing)}>
-						<input placeholder="On going Task" />
-						<button type="submit">Add ongoing Task</button>
-					</form>
-				</div>
-				<div className="header">
-					<form onSubmit={() => setCompleted(Completed)}>
-						<input placeholder="Finished Task" />
-						<button type="submit">Add Completed Tasks</button>
-					</form>
+			<div>
+				{tasks.map((task) => {
+					if (id === task.projectId) {
+						return (
+							<div className="task">
+								<h3>{task.title}</h3>
+								<p>{task.comment}</p>
+								<p>{task.status}</p>
+							</div>
+						);
+					}
+				})}
+				<div className="addTask">
+					<h2>Add a new Task </h2>
+					<input type="text" onChange={(e) => setTitle(e.target.value)} placeholder="task title" required />
+					<textarea type="text" onChange={(e) => setComment(e.target.value)} placeholder="comment" />
+					<button onClick={addTask}>add</button>
 				</div>
 			</div>
 		</div>

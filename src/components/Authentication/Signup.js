5c4e60905fe09ccf3ca62ca4881a9d3d9fd0d569
Elias Khoury <eliaskhouryy@hotmail.com>
2022-09-05 11:53:21 +1000

    snapshot

--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -8,6 +8,7 @@ const Signup = () => {
 	const [email, setEmail] = useState('');
 	const [error, setError] = useState('');
 	const [password, setPassword] = useState('');
+	const [confirm, setConfirm] = useState('');
 	const { signUp } = useUserAuth();
 	let navigate = useNavigate();
 
@@ -15,6 +16,10 @@ const Signup = () => {
 		//hangles the sign up form and sends the data back to firebase with the "signUp" firebase function
 		e.preventDefault();
 		setError('');
+		if (password !== confirm) {
+			alert('passwords do not match');
+			return;
+		}
 		try {
 			await signUp(email, password);
 			navigate('/');
@@ -29,9 +34,10 @@ const Signup = () => {
 				<h2>Signup</h2>
 				{error && <Alert variant="danger">{error}</Alert>}
 				<form onSubmit={handleSubmit}>
-					<input type="email" placeholder="Email address" onChange={(e) => setEmail(e.target.value)} />
+					<input type="email" required placeholder="Email address" onChange={(e) => setEmail(e.target.value)} />
 
-					<input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+					<input type="password" required placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+					<input type="password" required placeholder="Confirm Password" onChange={(e) => setConfirm(e.target.value)} />
 
 					<div className="d-grid gap-2">
 						<button variant="primary" type="Submit">
--- src/components/Taskr/Project.js
+++ src/components/Taskr/Project.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useUserAuth } from '../context/UserAuthContext';
-import { collection, getDocs, addDoc, updateDoc, doc } from 'firebase/firestore';
+import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from 'firebase/firestore';
+import { onSnapshot } from 'firebase/firestore';
 import { db } from '../firebase';
 import { useDrag } from 'react-dnd';
 import { useDrop } from 'react-dnd';
-
+import NavBar from './NavBar';
 
 import './project.scss';
 
-function Project(){
+function Project() {
 	let { id } = useParams();
 	const { logOut, user } = useUserAuth();
 	const [title, setTitle] = useState('');
@@ -19,89 +20,98 @@ function Project(){
 	const projectsRef = collection(db, 'projects');
 	const tasksRef = collection(db, 'tasks');
 	const [projectDetails, setProjectDetails] = useState([]);
-    const [board,setBoard] = useState([])
+	const [board, setBoard] = useState([]);
 
+	const [{ isDragging }, drag] = useDrag(() => ({
+		type: 'task',
+		item: tasks[0],
+		collect: (monitor) => ({
+			isDragging: !!monitor.isDragging(),
+		}),
+	}));
 
-    
-    const [{isDragging}, drag] = useDrag(() => ({
-        type:"task",
-        item: {id: tasks[0]},
-        collect: (monitor) => ({
-            isDragging: !!monitor.isDragging()
-        })    
-    }))
-    
-    const [{isOver},drop] = useDrop(() => ({
-        accept: "task",
-        drop: (item) => addTaskToDone(item.id),
-        collect: (monitor) => ({
-            isOver: !!monitor.isOver(),
-
-        })
-    }))
-    
-    const addTaskToDone = (id) => {
-        console.log(id)
-        // const taskList = tasks.filter((task) => id === task.id)
-        // setBoard((board) => [...board, taskList[0]])
-    }
-
-	const addTask = async () => {
-		await addDoc(tasksRef, { projectId: id, title: title, comment: comment, status: false });
-		window.location.reload();
+	const [{ isOver }, drop] = useDrop(() => ({
+		accept: 'task',
+		drop: (item) => addTaskToDone(item.id),
+		collect: (monitor) => ({
+			isOver: !!monitor.isOver(),
+		}),
+	}));
+	const handleDelete = async (id) => {
+		const projectsRef = doc(db, 'tasks', id);
+		try {
+			await deleteDoc(projectsRef);
+		} catch (err) {
+			alert(err);
+		}
+	};
+	const addTaskToDone = (id) => {
+		console.log(id);
+		// const taskList = tasks.filter((task) => id === task.id)
+		// setBoard((board) => [...board, taskList[0]])
 	};
 
-	useEffect(() => {
-		const getProjectDetails = async () => {
-			const data = await getDocs(projectsRef);
-			setProjectDetails(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-		};
-		getProjectDetails();
-		const getTaskDetails = async () => {
-			const data = await getDocs(tasksRef);
-			setTasks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-		};
-		getTaskDetails();
-	}, []);
+	const addTask = async (e) => {
+		e.preventDefault();
+		await addDoc(tasksRef, { projectId: id, title: title, comment: comment, status: false });
+		setTitle('');
+		setComment('');
+	};
+
+	useEffect(
+		() => onSnapshot(collection(db, 'tasks'), (snapshot) => setTasks(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))),
+
+		() => onSnapshot(collection(db, 'projects'), (snapshot) => setProjectDetails(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))),
+		[]
+	);
 
 	return (
-            <div className="newProject">
-                {projectDetails.map((project) => {
-                    return <h1>{project.id === id ? project.title : ''}</h1>;
-                })}
-                <div>
-                    {tasks.map((task) => {
-                        if (id === task.projectId) {
-                            return (
-                                <div className="task" key={task.id} ref={drag} id={task.id} setBoard={task.id}>
-                                    <h3>{task.title}</h3>
-                                    <p>{task.comment}</p>
-                                    <p>{task.status}</p>
-                                </div>
-                            );
-                        }
-                    })}
-                     
-                    <div className="addTask">
-                        <h2>Add a new Task </h2>
-                        <input type="text" onChange={(e) => setTitle(e.target.value)} placeholder="task title" required />
-                        <textarea type="text" onChange={(e) => setComment(e.target.value)} placeholder="comment" />
-                        <button onClick={addTask}>add</button>
-                    </div>
-                </div>
-                <div className='done' ref={drop}>
-                    {board.map((task) => {
-                        return  (
-                            <div key={task.id} id={task.id}>
-                                <h3>{task.title}</h3>
-                                    <p>{task.comment}</p>
-                                    <p>{task.status}</p>
-                            </div>
-                        )
-                    })}    
-                </div>
-            </div> 
+		<div className="newProject">
+			<NavBar />
+			{projectDetails.map((project) => {
+				return <h1>{project.id === id ? project.title : ''}</h1>;
+			})}
+			<div>
+				{tasks.map((task) => {
+					if (id === task.projectId) {
+						return (
+							<div className="task" key={task.id} ref={drag} id={task.id} setBoard={task.id}>
+								<h3>{task.title}</h3>
+								<p>{task.comment}</p>
+								<p>{task.status}</p>
+								<div
+									className="close"
+									onClick={() => {
+										handleDelete(task.id);
+									}}
+								></div>
+							</div>
+						);
+					}
+				})}
+
+				<div className="addTask">
+					<h2>Add a new Task </h2>
+					<form onSubmit={addTask}>
+						<input required value={title} type="text" onChange={(e) => setTitle(e.target.value)} placeholder="task title" required />
+						<textarea required value={comment} type="text" onChange={(e) => setComment(e.target.value)} placeholder="comment" />
+						<button>add</button>
+					</form>
+				</div>
+			</div>
+			<div className="done" ref={drop}>
+				{board.map((task) => {
+					return (
+						<div key={task.id} id={task.id}>
+							<h3>{task.title}</h3>
+							<p>{task.comment}</p>
+							<p>{task.status}</p>
+						</div>
+					);
+				})}
+			</div>
+		</div>
 	);
-};
+}
 
 export default Project;
